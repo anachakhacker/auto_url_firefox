0cce8ae6fefd19bd170fa1ccb289308dc368aad9
AKTheRipper <30810149+anachakhacker@users.noreply.github.com>
2021-11-13 16:16:45 +0100

    Add files via upload

---
+++ AKRUL.bat
@@ -0,0 +1,18 @@
+@ ECHO off
+echo.
+echo Made By AnachakHacker
+echo.
+set /p URL="Enter Twitch URL: "
+echo.
+cd..
+start firefox -P 11 -no-remote %URL%
+start firefox -P 12 -no-remote %URL%
+start firefox -P 13 -no-remote %URL%
+start firefox -P 14 -no-remote %URL%
+start firefox -P 15 -no-remote %URL%
+start firefox -P 16 -no-remote %URL%
+start firefox -P 17 -no-remote %URL%
+start firefox -P 18 -no-remote %URL%
+start firefox -P 19 -no-remote %URL%
+start firefox -P 20 -no-remote %URL%
+pause
